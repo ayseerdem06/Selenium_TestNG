8d6702ce4b4cc63ec7ae22d001ab356e523c2f30
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-09-27 23:34:56 +0300

    27 Eylul

---
+++ src/test/java/utilities/TestBaseBeforeClassAfterClass.java
@@ -0,0 +1,31 @@
+package utilities;
+
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.time.Duration;
+
+public abstract class TestBaseBeforeClassAfterClass {
+
+    //// @BeforeClass ve @AfterClass notasyonlarını TestNG de kullanırken JUnit'teki gibi static yapmaya gerek yoktur
+
+    protected static WebDriver driver;
+
+    @BeforeClass
+    public static void setup(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+
+    }
+
+    @AfterClass
+    public static void tearDown(){
+        driver.quit();
+    }
+}
