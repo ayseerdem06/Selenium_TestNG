0ed4a80193732dcad48085bc3c9e52673d03798a
Ayşe Erdem <aayseerdem0633@gmail.com>
2022-10-15 23:28:41 +0300

    15 Eylul

--- src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
+++ src/test/java/tests/day21/C01_ScreenShotReusableMethod.java
@@ -6,16 +6,23 @@ import pages.AmazonPage;
 import utilities.ConfigReader;
 import utilities.Driver;
 import utilities.ReusableMethods;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 
 public class C01_ScreenShotReusableMethod {
+
+    private static Logger logger = LogManager.getLogger(C01_ScreenShotReusableMethod.class.getName());
     @Test
     public void test01() throws IOException {
+        logger.info("Hepsi Burada sitesine gidilir");
         // HepsiBurada sayfasına gidiniz
         Driver.getDriver().get("https://www.hepsiburada.com");
+        logger.info("Ekran goruntusu alir");
         // Ve sayfanın resmini alınız
         ReusableMethods.getScreenshot("hepsiburada");
+        logger.info("Driver kapatılır");
         Driver.closeDriver();
     }
 
